c58d5ba16381e8ca998423e78d151a00b268d75c
wiktor <wiktor.janiszewski10@gmail.com>
2025-02-15 18:50:44 +0100

    feat: create simple router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,13 @@
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
-
+import { financeRouter } from "./routers/finance";
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  finance: financeRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
