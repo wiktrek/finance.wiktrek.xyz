8895373564560ca04ba87a98b17eb44a6bfbb957
wiktor <wiktor.janiszewski10@gmail.com>
2025-03-01 15:42:52 +0100

    feat: select from shadcn

--- src/app/_components/chart.tsx
+++ src/app/_components/chart.tsx
@@ -7,31 +7,40 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "~/components/ui/chart";
-
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
 import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
-const chartData = [
-  { month: "January", spent: 186, received: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
-];
-
-const chartConfig = {
-  desktop: {
-    label: "Spent",
-    color: "#2563eb",
-  },
-  mobile: {
-    label: "Received",
-    color: "#60a5fa",
-  },
-} satisfies ChartConfig;
-export function Chart() {
+interface ChartData {
+  month: string;
+  spent: number;
+  received: number;
+}
+export function SelectMenu() {
   return (
-    <ChartContainer config={chartConfig} className="h-64 w-96">
-      <BarChart accessibilityLayer data={chartData}>
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Theme" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="light">Light</SelectItem>
+        <SelectItem value="dark">Dark</SelectItem>
+        <SelectItem value="system">System</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+}
+export function Chart(props: {
+  chartConfig: ChartConfig;
+  chartData: ChartData[];
+}) {
+  return (
+    <ChartContainer config={props.chartConfig} className="h-64 w-96">
+      <BarChart accessibilityLayer data={props.chartData}>
         <CartesianGrid vertical={false} />
         <XAxis
           dataKey="month"
@@ -47,5 +56,12 @@ export function Chart() {
         <Bar dataKey="received" fill="var(--color-mobile)" radius={4} />
       </BarChart>
     </ChartContainer>
+  );
+}
+export function ChartWithSelectMenu(props: { id: string }) {
+  return (
+    <div>
+      <Chart chartConfig={} chartData={} />
+    </div>
   );
 }
